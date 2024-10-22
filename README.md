bbef6f9106bdaa80d8759180b5dc8e3f0281777f
prodzpod <product0339@gmail.com>
2024-10-23 00:38:48 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TILER2 no poll
+removes the auto polling feature from TILER2, improving fps drop by a lot (theoretically)
---
+++ TILER2NoPoll/Main.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using RoR2;
+using TILER2;
+
+namespace TILER2NoPoll
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(TILER2Plugin.ModGuid)]
+    public class Main : BaseUnityPlugin
+    {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "prodzpod";
+        public const string PluginName = "TILER2NoPoll";
+        public const string PluginVersion = "1.0.0";
+        public static ManualLogSource Log;
+        public static PluginInfo pluginInfo;
+        public static Harmony Harmony;
+
+        public void Awake()
+        {
+            pluginInfo = Info;
+            Log = Logger;
+            On.RoR2.RoR2Application.Update -= AutoConfigContainer.FilePollUpdateHook;
+            Harmony = new(PluginGUID);
+            Harmony.PatchAll(typeof(yo));
+        }
+    }
+    [HarmonyPatch(typeof(AutoConfigContainer), nameof(AutoConfigContainer.FilePollUpdateHook))]
+    public class yo
+    {
+        public static void Prefix()
+        {
+            Main.Log.LogInfo("AHHH (update is happening please let prod know)");
+        }
+    }
+}
